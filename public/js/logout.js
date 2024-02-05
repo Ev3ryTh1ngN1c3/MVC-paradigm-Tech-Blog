d991d62a2a6958544708baa8ae8af8d5a9419217
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-04 20:44:20 -0500

    added code for the logout functionality

---
+++ public/js/logout.js
@@ -0,0 +1,24 @@
+ // logout function to send request to log out the user
+const logout = async () => {
+    try {
+      const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/'); // when successful, load the homepage
+      } else {
+        throw new Error('failed to log out.'); // throw an error when unsuccessful
+      }
+    } catch (error) {
+      console.error(error);
+      alert('failed to log out.'); // show alert when there's an error
+    }
+  };
+  
+  // add an event listener to the logout button
+  const logoutButton = document.querySelector('#logout');
+  if (logoutButton) {
+    logoutButton.addEventListener('click', logout);
+  }
