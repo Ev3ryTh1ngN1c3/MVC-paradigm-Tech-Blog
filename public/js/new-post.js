9a2d260fd7ce6c641b9a776f8c233b7caefb63ab
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-04 20:54:12 -0500

    added code for creating a new post

---
+++ public/js/new-post.js
@@ -0,0 +1,35 @@
+ // create new post
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // get the values of the title & content input fields
+    const title = document.querySelector('#title-new-post').value.trim();
+    const content = document.querySelector('#content-new-post').value.trim();
+  
+    if (title && content) {
+      try {
+        // send a POST request to create a new post
+        const response = await fetch('/api/posts', {
+          method: 'POST',
+          body: JSON.stringify({ title, content }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+          document.location.replace('/dashboard'); // when successful, load the dashboard page
+        } else {
+          throw new Error('failed to create a new post'); // throw an error when unsuccessful
+        }
+      } catch (error) {
+        console.error(error);
+        alert('failed to create a new post'); // show alert when there's an error
+      }
+    }
+  };
+  
+  // event listeners
+  const newPostForm = document.querySelector('.new-post-form');
+  if (newPostForm) {
+    newPostForm.addEventListener('submit', newPostFormHandler); // add event listener for creating a new post
+  }
+  
