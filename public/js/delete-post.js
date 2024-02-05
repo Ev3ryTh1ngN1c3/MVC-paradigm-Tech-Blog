f5d192425f7fee077eb740bb31897012048e74f7
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-04 20:26:26 -0500

    added code for deleting a post

---
+++ public/js/delete-post.js
@@ -0,0 +1,31 @@
+// function to delete a post by its ID
+const deletePost = async (post_id) => {
+    try {
+      const response = await fetch(`/api/posts/${post_id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        document.location.reload(); // when successful, reload the page
+      } else {
+        throw new Error("Failed to delete the post."); // throw an error when unsuccessful
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Failed to delete the post."); // show alert when there's an error
+    }
+  };
+  
+  // event handler for deleting a post
+  const deletePostHandler = (event) => {
+    if (event.target.matches(".delete-post")) {
+      const post_id = event.target.getAttribute("data-post-id");
+      deletePost(post_id); // call deletePost function with the post ID
+    }
+  };
+  
+  document.addEventListener("click", deletePostHandler); // add event listener to the document  
+  
+  
+  
